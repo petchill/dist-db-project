dcf7502cdb78424b5e597151c83e3790a56409e5
songpondev <songpon111333@gmail.com>
2020-05-08 01:30:04 +0700

    edit room api

--- src/services/room.js
+++ src/services/room.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { rooms_model, tags_model, comments_model } from '../../models'
+import { rooms_model, users_model } from '../../models'
 import Joi from '@hapi/joi';
-import {Promise} from "bluebird";
+import { Promise } from "bluebird";
+import _ from 'lodash';
 Joi.objectId = require('joi-objectid')(Joi)
 
 const router = new Router();
@@ -15,33 +16,10 @@ const roomSchema = Joi.object().keys({
   room_tags: Joi.array().items(Joi.string())
 });
 
-async function addTag(room_id, tagName) {
-  try {
-    const response = await tags_model.findOneAndUpdate(
-      { tag_name: tagName },
-      {
-        $push: {
-          room_id: room_id
-        }
-      }
-    );
-    if (!response) {
-      const response = await tags_model.create({
-        tag_name: tagName,
-        room_id: [room_id]
-      });
-      return response._id;
-    }
-    return response._id;
-  } catch (error) {
-    console.error('add tag Error!', error);
-  }
-}
-
-// GET
+// GET all data
 router.get('/', async (req, res) => {
   try {
-    const data = await rooms_model.find({}).sort({createdAt:-1});
+    const data = await rooms_model.find({}).sort({ createdAt: -1 });
     console.log(data);
     res.status(200);
     res.json({ ok: true, status: 200, data: data });
@@ -52,19 +30,35 @@ router.get('/', async (req, res) => {
   }
 });
 
+// GET BY query
+router.post('/search', async (req, res) => {
+  const req_body = req.body;
+  console.log('///////this is req search: ', req_body)
+  try {
+    const data = await rooms_model.find(req_body);
+    res.status(200);
+    res.json({ ok: true, status: 200, data: data })
+  } catch (error) {
+    console.error(error);
+    res.status(200);
+    res.json({ ok: false, status: 400, error: error });
+  }
+})
+
 // GET BY ID
 router.get('/:id', async (req, res) => {
   try {
     const data = await rooms_model.findById(req.params.id);
     res.status(200);
-    res.json({ ok: true, status: 200, data: data})
+    res.json({ ok: true, status: 200, data: data })
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(400);
     res.json({ ok: false, status: 400, error: error });
   }
 });
 
+POST ( createRoom )
 router.post('/', async (req, res) => {
   const req_body = req.body;
   const { value: data, error: validated_error } = roomSchema.validate(req_body, joi_option);
@@ -76,22 +70,11 @@ router.post('/', async (req, res) => {
     const { room_tag: roomTags, ...payload_data } = data;
     let response = await rooms_model.create(payload_data);
     const roomId = response._id;
-    if (roomTags) {
-      const tagList = await Promise.map(roomTags, async (curr_tag) => {
-        const curr_tag_id = await addTag(roomId, curr_tag);
-        console.log('/////////////this is curr_tag_id', curr_tag_id);
-        return curr_tag_id;
-      })
-      console.log('///////tagList : ', tagList);
-      response = await rooms_model.findOneAndUpdate(
-        { '_id': roomId },
-        {
-          $push: {
-            room_tag_id: tagList
-          }
-        }
-      );
-    }
+    const userUpdateRoom = await users_model.findByIdAndUpdate(payload_data.owner_id, {
+      $push: {
+        room_id: roomId
+      }
+    })
     res.status(200);
     res.json({ ok: true, status: 200, data: response });
   } catch (error) {
